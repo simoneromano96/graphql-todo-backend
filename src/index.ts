d31b11e439fb6b0f3e74f83dcbcdd8ef47f0c318
Simono il magnifico <simoneromano@tutanota.de>
2020-12-02 19:19:39 +0100

    BROKEN update deps

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,18 @@ const main = async () => {
   // DEBUG mode, this will show the queries to the db
   mongoose.set("debug", true)
 
+  console.log("Config")
+  console.log({ ...config })
+
+  console.log("Mongo init")
+
   await mongoose.connect(config.app.db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
+
+  console.log("Mongo initialised")
 
   const app = Fastify()
 
@@ -22,12 +29,17 @@ const main = async () => {
     origin: config.cors.origin,
   })
 
+  console.log("CORS initialised")
+
   app.register(mercurius, {
     schema,
     subscription: true,
-    graphiql: "playground",
-    prefix: config.app.apiPrefix,
+    // graphiql: "playground",
+    // path: "/graphql",
+    // prefix: config.app.apiPrefix,
   })
+
+  console.log("API initialised")
 
   await app.listen(config.app.port, "0.0.0.0")
 }

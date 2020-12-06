8b4236bf45a09492baf17f9467c2fc19d0d0ab9d
Simono il magnifico <simoneromano@tutanota.de>
2020-12-06 15:55:50 +0100

    test auth

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,9 @@ export default {
     port: process.env.APP_PORT ?? 3000,
     db: process.env.APP_DB ?? "mongodb://root:example@localhost",
     apiPrefix: process.env.APP_API_PREFIX,
+    session: {
+      secret: process.env.APP_SESSION_SECRET ?? "CNLxr58XzCaZuodxfZxQsOCRxTRrCki5",
+    },
   },
   cors: {
     origin: process.env.APP_CORS_ORIGIN ?? "*",
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import Fastify from "fastify"
 import cors from "fastify-cors"
-import mongoose from "mongoose"
+// SESSION
+import fastifyCookie from "fastify-cookie"
+import fastifySession from "fastify-session"
+import connectRedis from "connect-redis"
+import redis from "redis"
+
 import mercurius from "mercurius"
+import mongoose from "mongoose"
 
 import { schema } from "./schema"
 import config from "./config"
+
+const redisClient = redis.createClient()
+
+const RedisStore = connectRedis(fastifySession as any)
 
 const main = async () => {
   // DEBUG mode, this will show the queries to the db
@@ -22,11 +32,62 @@ const main = async () => {
     origin: config.cors.origin,
   })
 
+  app.register(fastifyCookie)
+  app.register(fastifySession, {
+    secret: config.app.session.secret,
+    store: new RedisStore({
+      host: "localhost",
+      port: 6379,
+      client: redisClient,
+      ttl: 600,
+    }),
+    cookieName: "sesId",
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      domain: "localhost",
+      path: "/",
+    },
+  })
+
   app.register(mercurius, {
     schema,
     subscription: true,
     graphiql: "playground",
     prefix: config.app.apiPrefix,
+  })
+
+  // Login route
+  app.post("/login", (request, reply) => {
+    const { email, password } = request.body as any
+
+    if (password === "password") {
+      request.session.authenticated = true
+      reply.send({ authenticated: true })
+    } else {
+      reply.send({ error: true, message: "Wrong password, try password" })
+    }
+  })
+
+  // Get session route
+  app.get("/session", (request, reply) => {
+    reply.send({ ...request.session })
+  })
+
+  // Logout route
+  app.get("/logout", (request, reply) => {
+    if (request.session.authenticated) {
+      request.destroySession((err) => {
+        if (err) {
+          reply.status(500)
+          reply.send("Internal Server Error")
+        } else {
+          reply.send({ message: "Logged out" })
+        }
+      })
+    } else {
+      reply.send({ error: true, message: "You're not logged in" })
+    }
   })
 
   await app.listen(config.app.port, "0.0.0.0")
